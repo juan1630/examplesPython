b64258da42b7619cb171f0339dcf98da970d52d0
Juan Patron <josejuanpatron1630@gmail.com>
2021-07-04 22:03:18 -0500

    Se agrego el contador de los archivos que se movieronç

--- respaldoDocs.py
+++ respaldoDocs.py
@@ -1,17 +1,27 @@
 import shutil
 import os
 
+
 # se convierte a una lista el resultado de buscar dentro de la carpeta
 count = list(os.walk("C:\\projects\\back\horizonte\\horizonte\\contratos\\img"))
-# print(count)
+print(len(count))
+# Sirve para escribir en la consola
 
+list1 = []  
 def respaldoFiles():
+    conta1 = 0
     #itero cada uno de los resultados
     for img in count:
         files = img[2]
+        conta2 = len(files)
+
         for file in files:
-            print(file)
+            conta1 += 1
+            number = ( 100 * conta1)/conta2
+            numerberRound = round( number, 2 )
             shutil.copy( os.path.join("C:\\projects\\back\horizonte\\horizonte\\contratos\\img\\", file) ,  "C:\\Users\\juanp\\OneDrive\\Escritorio\\respaldo")
+            print(numerberRound)
+
             # la funcion que se encarga de copiar cada uno de los archivos
 
 
