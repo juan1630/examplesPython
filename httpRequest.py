fc91044e29b275a445eef907800923a5b05f20a3
Juan Patrón <josejuanpatron1630@gmail.com>
2022-03-08 23:23:37 -0600

    Script de pruebas

--- httpRequest.py
+++ httpRequest.py
@@ -1,8 +1,20 @@
 import requests
 
-# url = "https://blooming-sea-53514.herokuapp.com/agregar/modulo"
-url = "http://localhost:3200/flebotomia/prueba/algo"
+url = "https://backend-laboratorios.herokuapp.com/api/register/product"
+#url = "http://localhost:3200/flebotomia/prueba/algo"
 
-responseServer = requests.get( url )
-data = responseServer.json()
-print( data )
+def sendData(i):
+    dataJosn = {
+        "ESTUDIO":"Prueba",
+        "PRECIO_PUBLICO":200
+    }
+
+    responseServer = requests.post( url ,dataJosn )
+    dataResp = responseServer.json()
+
+    print( responseServer)
+    print (i)
+    print (dataResp)
+
+for i in range(100):
+    sendData(i);
