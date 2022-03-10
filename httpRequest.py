83d83b44fc4805b4113a7d475976aaf242817042
Juan Patrón <josejuanpatron1630@gmail.com>
2022-03-09 22:25:15 -0600

    script de peticiones

--- httpRequest.py
+++ httpRequest.py
@@ -1,7 +1,19 @@
 import requests
 
-url = "https://backend-laboratorios.herokuapp.com/api/register/product"
+#url = "https://backend-laboratorios.herokuapp.com/api/register/pacientes"
 #url = "http://localhost:3200/flebotomia/prueba/algo"
+#url = "http://localhost:3000/api/list/pacientes"
+#url = "https://backend-laboratorios.herokuapp.com/api/list/pacientes"
+#url = "https://backend-laboratorios.herokuapp.com/api/register/pacientes"
+url = "http://localhost:3000/api/register/pacientes"
+
+def getPacientes(i):
+    responseServerPacientes = requests.get( url )
+    dataPacientesJson = responseServerPacientes.json()
+    print(dataPacientesJson)
+    print(i)
+
+
 
 def sendData(i):
     dataJosn = {
@@ -16,5 +28,28 @@ def sendData(i):
     print (i)
     print (dataResp)
 
+
+
+def registerPacientes(i):
+    dataJosnPaciente = {
+    "nombrePaciente":"Juan Prueba 1",
+    "genero":"masculino",
+    "edad":25,
+    "curp":"xxxxxxxxxxxxxxxxxxxxxxxx",
+    "correoPaciente":"juan@gmail.com    ",
+    "estadoPaciente":"Morelos",
+    "tipoDeSangre":"A+"
+}
+
+    responseServerPacientes = requests.post( url ,dataJosnPaciente )
+    dataRespPacientes = responseServerPacientes.json()
+
+    print( responseServerPacientes)
+    print (i)
+    print (dataRespPacientes)
+
+
 for i in range(100):
-    sendData(i);
+    #sendData(i)
+    registerPacientes(i)
+    #getPacientes(i)
