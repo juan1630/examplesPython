9b044c668dac4d69aa424c2e1cc6b4272b008d37
Juan Patrón <josejuanpatron1630@gmail.com>
2022-07-20 23:45:16 -0500

    Reciclaje de las fuciones para testear las api

--- httpRequest.py
+++ httpRequest.py
@@ -6,7 +6,7 @@ import requests
 #url = "https://backend-laboratorios.herokuapp.com/api/list/pacientes"
 #url = "https://backend-laboratorios.herokuapp.com/api/register/pacientes"
 # url = "http://localhost:3000/api/register/pacientes"
-# url = "http://localhost:3000/api/register/product"
+#url = "http://localhost:3000/api/register/product"
 # url = "http://localhost:3000/api/new/venta"
 # url = "http://localhost:3000/api/new/pedido"
 # url = "http://localhost:3000/auth/register/personal"
@@ -15,8 +15,17 @@ import requests
 #url = "https://backend-laboratorios.herokuapp.com/api/new/pedido"
 # url = "http://localhost:3200/register/nota/indicaciones"
 # url = "http://localhost:3000/api/list/paciente/626dab5ee648bb1faac74756"
-url = "http://localhost:3000/api/list/estudios/activos/6270b26d1c706aed7e2ed179"
+# url = "http://localhost:3000/api/list/estudios/activos/6270b26d1c706aed7e2ed179"
+#url = "http://tecgramapi.southcentralus.cloudapp.azure.com:5000/#portfolio"
+url = 'http://localhost:3200'
+#url = 'Se debe agregar la url PRD'
+endPoint = '/ingreso/hospitalizacion'
+urlEndpoint = url + endPoint
 
+print(urlEndpoint)
+
+
+jsonTest = {}
 
 def getPacientes(i):
     responseServerPacientes = requests.get( url )
@@ -159,8 +168,52 @@ def getEstudiosActivos(i):
     print( dataResponseEstudios )
     print(i)
 
+def registerEstudiosNuevos(i):
+    dataRegister = {
+        "ESTUDIO":"Prueba",
+        "PRECIO_PUBLICO":200
+    }
+    responseRegister = requests.post( url, dataRegister)
+    dataResponseRegister = responseRegister.json()
+    print( dataResponseRegister )
 
-for i in range(200):
+def getData():
+    responseRegister = requests.get( url)
+    print( responseRegister )
+        
+
+def ingresoHospitalizacion():
+    hospi =  {
+        "iagnosticoInicial": "Algo muy malo para operar",
+        "interrogatorio":"Si",
+        "plan": "Algo de lo que sea",
+        "enfermeraAtendio": "5e83a3881be2ff1b28408e9c",
+        "idPaciente": "5fcfebf40b82bb00175f2ffb",
+        "medicoIngreso": "61ba38936722ff03e6aa68f0",
+        "medicoTratante": "61ba38936722ff03e6aa68eb"
+        }
+    responseGetHospi =  requests.post(urlEndpoint, hospi)
+    dataHospiResonse = responseGetHospi.json()
+    print( dataHospiResonse )
+
+
+#TODO! NUEVAS FUCIONES PARA PODER CREAR PRUEBAS DE MANERA MAS RAPIDS, SE AGREGO LA FUNCION REGISTEFILEDS Y LA GETDATOS
+# ? PARA CREAR LAS PRUEBAS ES NECESARIO QUE SE MODIFIQUE EL JOSNTEST POR EL QUE SE QDEBE ENVIAR AL API
+
+def registerFileds():
+    responsePost =  requests.post(urlEndpoint, jsonTest)
+    dataHospiResonse = responsePost.json()
+    print( dataHospiResonse )
+
+
+
+def getDatos():
+
+    responseServer = requests.get( urlEndpoint )
+    dataPacientesJson = responseServer.json()
+    print(dataPacientesJson)
+
+for i in range(1000):
     # sendData(i)
     # registerPacientes(i)
     #getPacientes(i)
@@ -170,4 +223,7 @@ for i in range(200):
     # registerNewVenta(i)
     # registerNotasMedicas(i)
     # getPacienteById(i)
-    getEstudiosActivos(i)
+    # getEstudiosActivos(i)
+    #registerEstudiosNuevos(i)
+    # getData()
+    ingresoHospitalizacion()
